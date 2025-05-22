25d7a30444e4e3673336efff9255971ca426d791
dreadster3 <afonso.antunes@live.com.pt>
2025-05-22 22:53:45 +0100

    feat: icmp packet parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "log.h"
 #include "net/ethernet.h"
+#include "net/icmp.h"
 #include "net/ipv4.h"
 #include "tun.h"
 #include <csignal>
@@ -23,24 +24,47 @@ int main() {
 
       std::span<const uint8_t> packet;
       auto ethernet_header = net::ethernet::parse_header(frame, packet);
-      if (ethernet_header) {
-        LOG_INFO("Ethernet packet received");
-        LOG_DEBUG("Header: {}", ethernet_header->to_string());
 
-        switch (ethernet_header->type) {
-        default:
-          LOG_WARN("Packet type {} not supported",
-                   net::ethernet::packet_type_to_string(ethernet_header->type));
-          break;
-        case net::ethernet::PacketType::IPv4:
-          std::span<const uint8_t> payload;
-          auto ipv4_header = net::ethernet::ipv4::parse(packet, payload);
-          if (ipv4_header) {
-            LOG_INFO("IPv4 packet received");
-            LOG_DEBUG("Header: {}", ipv4_header->to_string());
+      if (!ethernet_header)
+        continue;
+
+      LOG_INFO("Ethernet packet received");
+      LOG_DEBUG("Ethernet Header: {}", ethernet_header->to_string());
+
+      switch (ethernet_header->type) {
+      case net::ethernet::PacketType::IPv4: {
+        std::span<const uint8_t> ipv4_data;
+        auto ipv4_header = net::ethernet::ipv4::parse(packet, ipv4_data);
+        if (ipv4_header) {
+          LOG_INFO("IPv4 packet received");
+          LOG_DEBUG("IPv4 Header: {}", ipv4_header->to_string());
+          switch (ipv4_header->protocol) {
+          case net::ethernet::ipv4::Protocol::ICMP: {
+            std::span<const uint8_t> icmp_data;
+
+            auto icmp_header =
+                net::ethernet::ipv4::icmp::parse(ipv4_data, icmp_data);
+            if (!icmp_header) {
+              continue;
+            }
+
+            LOG_INFO("ICMP packet received");
+            LOG_DEBUG("ICMP Header: {}", icmp_header->to_string());
+            break;
           }
-          break;
+          default:
+            LOG_WARN(
+                "IPv4 protocol {} not supported",
+                net::ethernet::ipv4::protocol_to_string(ipv4_header->protocol));
+            break;
+          }
         }
+        break;
+      }
+      default:
+        LOG_WARN("Packet type {} not supported",
+                 net::ethernet::packet_type_to_string(ethernet_header->type));
+        break;
       }
 
       n = tap.write(frame);
---
+++ src/net/icmp.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <cstdint>
+#include <format>
+#include <optional>
+#include <span>
+#include <string>
+
+namespace net::ethernet::ipv4::icmp {
+enum class PacketType : uint8_t { Reply = 0x00, Echo = 0x08, Unknown = 0x00 };
+
+inline std::string type_to_string(PacketType type) {
+  switch (type) {
+  case PacketType::Echo:
+    return "Echo";
+  case PacketType::Reply:
+    return "Reply";
+  default:
+    return "Unknown";
+  }
+};
+
+inline PacketType to_type(uint8_t type) {
+  switch (type) {
+  case 0x08:
+    return PacketType::Echo;
+  case 0x00:
+    return PacketType::Reply;
+  default:
+    return PacketType::Unknown;
+  }
+}
+
+#pragma pack(push, 1)
+struct Header {
+  PacketType type;
+  uint8_t code;
+  uint16_t checksum;
+  uint16_t identifier;
+  uint16_t sequence_number;
+
+  std::string to_string() const {
+    return std::format("ICMP(type={}, code={}, checksum={}, identifier={}, "
+                       "sequence_number={})",
+                       type_to_string(type), code, checksum, identifier,
+                       sequence_number);
+  }
+};
+#pragma pack(pop)
+
+std::optional<Header> parse(std::span<const uint8_t> packet,
+                            std::span<const uint8_t> &payload) {
+  if (packet.size() < sizeof(Header)) {
+    return std::nullopt;
+  }
+
+  Header header{};
+  header.type = to_type(packet[0]);
+  header.code = packet[1];
+  header.checksum = (packet[2] << 8) | packet[3];
+  header.identifier = (packet[4] << 8) | packet[5];
+  header.sequence_number = (packet[6] << 8) | packet[7];
+
+  // Checksum
+
+  payload = packet.subspan(8);
+  return header;
+}
+
+} // namespace net::ethernet::ipv4::icmp
